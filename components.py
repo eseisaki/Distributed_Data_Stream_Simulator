be2fa83bf7bb6db90e3c0cb6858534dbc6a3f75d
Eftychia Seisaki <44266465+eseisaki@users.noreply.github.com>
2020-02-12 17:53:52 +0200

    create new sim with protocol, proxy

--- components.py
+++ components.py
@@ -1,267 +1,182 @@
-"""Distributed stream system architecture simulation classes.
-
-The purpose of these classes is to collect detailed statistics
-that are independent of the particular algorithm, and report
-them in a standardized (and therefore auto-processable) manner."""
-
-__docformat__ = 'reStructuredText'
-
-import sys
+import functools
 
 
-# -----------------------------------------------------------------------------
+###############################################################################
 #
-#   Channels
+# Decorators
 #
-# -----------------------------------------------------------------------------
+###############################################################################
+def remote_class(ifc):
+    def decorator_remote(cls):
+        @functools.wraps(cls)
+        def wrapper_decorator(*args, **kwargs):
+            instance = cls(*args, **kwargs)
+            if ifc in instance.net.protocol.interfaces:
+                interface = instance.net.protocol.interfaces[ifc]
+                for name in interface.methods.keys():
+                    obj = getattr(instance, str(name), None)
+                    if not callable(obj):
+                        raise TypeError(f"The {str(name)!r} function must be "
+                                        f"implemented")
+            return instance
 
+        return wrapper_decorator
+
+    return decorator_remote
+
+
+###############################################################################
+#
+# Channels
+#
+###############################################################################
 class Channel:
-    """Point-to-point or broadcast unidirectional channel.
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
 
-       Channels are used to collect network statistics. Each channel counts
-       the number of messages and the total message size (in bytes).
-
-       Message types are defined by a "string" same as the name of the
-       endpoint.
-
-       Each endpoint is associated with a request channel, and --if it is not
-       one-way--- with a response channel.
-
-
-       :type src: host
-       :param src: the one that sends the msg
-       :type dst: host
-       :param dst: the one that receives the msg
-       :type rpcc: string
-       :param rpcc: the code of the endpoint associated with the channel
-       :type msg: int
-       :param msg: number of msgs sent by this channel
-       :type bytes: int
-       :param bytes: bytes sent by this channel
-    """
-
-    def __init__(self, src, dst, rpcc):
-        """
-        creates the associated parameters of a channel object
-
-        :param src: the one that sends the msg
-        :param dst: the one that receives the msg
-        """
-
-        self.__src = src  # source node
-        self.__dst = dst  # destination node
-        self.__rpcc = rpcc  # rpcc code
-
-        self.__msg = 0  # no of msg sent
-        self.__bytes = 0  # no of bytes sent
-
-    def msg_sent(self):
-        """
-        Aggregates msg sent and their bytes
-        :return:msgs
-        """
-        return self.msg
-
-    def msg_received(self):
-        """
-        Aggregates msg received and their bytes. For broadcast channels, this
-        is not the same as bytes sent.
-        :return: bytes
-        """
-        return self.bytes
+        self.msg = 0
+        self.bytes = 0
 
     def transmit(self, msg_size):
-        """
-        Register the transmission of a message on this channel
-
-        :param msg_size:the number of bytes in the transmitted msg
-        :return: None
-        """
         self.msg += 1
         self.bytes += msg_size
 
 
-class MulticastChannel(Channel):
-    """
-    A channel can be a multicast channel.
-
-    This is always associated with some one-way endpoint, sending data from a single source host A to
-    a destination host group B.Again, there are two channels associated with
-    A and B.One channel counts the traffic sent by A, and the second channel
-    counts the traffic received by all hosts in host group B. For example if
-    there are 3 hosts in group B, and a message of 100 bytes is sent, one
-    channel will register one additional message and 100 additional bytes,
-    and the other will register 3 additional messages and 300 additional
-    bytes.
-
-    :type rx_msg: int
-    :param rx_msg: total msgs received by all hosts in host_group
-    :type rx_bytes: int
-    :param rx_bytes:total bytes received by all hosts in host_group
-    """
-
-    def __init__(self, src, dst, rpcc):
-        super().__init__(src, dst, rpcc)
-
-        self.rx_msg = 0
-        self.rx_bytes = 0
-
-    def msg_received(self):
-        return self.rx_msg
-
-    def bytes_received(self):
-        return self.rx_bytes
-
-    def transmit(self, msg_size):
-        # TODO: find correct group size
-        group_size = dst.group_size
-        self.rx_msg += group_size
-        self.rx_bytes += group_size * msg_size
-
-
-# -----------------------------------------------------------------------------
+###############################################################################
 #
-#   Hosts
+# Hosts
 #
-# -----------------------------------------------------------------------------
+###############################################################################
+
 class Host:
-    """
-    Hosts are used as nodes in the network.
-
-    A host can represent a single network destination(site),or a set of network
-    destinations.Any subclass of host is a single site.For broadcast sites one,
-    has to use "HostGroup" class.
-
-    :type nid: int (key) :param nid: the unique id of a host
-    :type peers: set :param peers: includes nids of all possible destinations
-    :type recv_channels: dict :param recv_channels: a list of all recv channels
-    :type send_channels: dict :param send_channels: a list of all recv channels
-    :type handlers: dict :param handlers: a list of all handler methods
-    """
-
-    def __init__(self, net, nid, is_bcast):
-        """
-        creates a host object given an id
-
-        :param nid: unique id of a host
-        """
-        self.net = net  # The network the host belongs to
-        self.nid = nid  # The unique id of the host
-        self.is_bcast = is_bcast  # True if this is a host group
-        self.incoming_channels = {}  # The channels associated with the host
-
-        if not self.is_bcast:
-            self.net.hosts[self.nid] = self
-        else:
-            self.net.groups[self.nid] = self
-
-    def delete(self):
-        if not self.is_bcast:
-            del self.net.hosts[self.nid]
-        else:
-            del self.net.groups[self.nid]
+    def __init__(self, net):
+        self.net = net
+        self.incoming_channels = {}
 
 
-class HostGroup(Host):
-    """
-    A host group represents a broadcast address.
-
-    This is simply an abstract base class.The implementation of this class can
-    be anything.All that this class interface provides is the methods that are
-    required by the communication traffic computation.
-    """
-
-    def __init__(self, net, nid, is_bcast=True):
-        super.__init__(nid, is_bcast)
-        self.add_member(self)
-
-        self.members = {}  # the members of the group
-
-    def add_member(self, node):
-        """
-        Adds a host in the host group.
-
-        :param node: the node to be added
-        :return: None
-        """
-        self.members[node.nid] = node
-
-    def remove_member(self, node):
-        """
-        Removes a host from the host group
-        :param node: the node to be removed
-        :return: None
-        """
-        del self.members[node.nid]
-
-    def group_size(self):
-        """
-        Calculates the size of the host group.
-
-        :return:group size
-        """
-        return len(self.members)
-
-
-# -----------------------------------------------------------------------------
+###############################################################################
 #
-#   RPC PROTOCOL
+# Protocol
 #
-# -----------------------------------------------------------------------------
-class RemoteMethod:
-    """Represents a method that host can call on other hosts.
-
-    This type basically only holds the name of the remote method and whether is
-    one-way or not."""
-    pass
-
+###############################################################################
 
 class Interface:
-    """
-    It is like a remote type.It represents a collection of remote methods
-    that are implemented on a remote host
-    """
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.methods = {}
+
+    def add_method(self, name, one_way):
+        if name not in self.methods:
+            self.methods[name] = one_way
 
 
 class Protocol:
-    """
-    A collection of rpc interfaces.
+    def __init__(self):
+        self.interfaces = {}
 
-    A protocol is the collection of RPC interfaces used in a network.
-    """
-    pass
+    def add_interface(self, ifc, methods):
+        for key, value in methods.items():
+            self.add_method(ifc, key, value)
+
+    def add_method(self, ifc, name, one_way):
+        if ifc not in self.interfaces:
+            self.interfaces[ifc] = Interface(ifc)
+        self.interfaces[ifc].add_method(name, one_way)
 
 
 class Endpoint:
-    """
-    The implementation of a remote method on a remote host
+    def __init__(self, func, req_channel, resp_channel=None):
+        self.send = func
+        self.req_channel = req_channel
+        self.resp_channel = resp_channel
 
-    An rpc call belongs to some specific rpc proxy.
-    """
-    pass
 
+###############################################################################
+#
+# Proxy
+#
+###############################################################################
 
 class Proxy:
-    """
-    Represents a proxy object for some host
-
-    When a host A wants to call a remote method on host B, it makes the call
-    through a proxy method, so that the network traffic can be accounted for.
-    Host A is the owner of the proxy and host B is the proxied host.
-
-    Each proxy is associated with an rpc interface, which represents the
-    collection of endpoints being proxied.
-    """
-
-    def __init__(self, interface, owner):
-        self.interface = interface
+    def __init__(self, ifc):
+        self.ifc = ifc
+        self.owner = None
+        self.proxied = None
         self.endpoints = {}
-        self.owner = owner
-        self.remote_proc = {}  # the node being proxied
 
-    def add_endpoint(self, msgtype, func):
-        self.endpoint[msgtype] = func
+    def create_endpoints(self):
+        ifc_obj = self.owner.net.protocol.interfaces[self.ifc]
 
-    def connect(self, dst):
-        self.remote_proc[dst.nid] = dst
+        for name, one_way in ifc_obj.methods.items():
+            send = getattr(self.proxied, name)
+            req_channel = Channel(self.owner, self.proxied)
+            if not one_way:
+                self.endpoints[name] = Endpoint(send, req_channel)
+            else:
+                resp_channel = Channel(self.proxied, self.owner)
+                self.endpoints[name] = Endpoint(send, req_channel,
+                                                resp_channel)
+
+
+class Sender(Host):
+    def __init__(self, net, ifc):
+        super().__init__(net)
+        if ifc not in self.net.protocol.interfaces:
+            raise TypeError(f"There is no {ifc!r} interface")
+        self.proxy = Proxy(ifc)
+
+    def connect_proxy(self, proxied):
+        if proxied not in self.net.hosts:
+            TypeError(f"There is no {proxied!r} host")
+
+        self.proxy.owner = self
+        self.proxy.proxied = proxied
+
+        self.proxy.create_endpoints()
+
+    def send(self, method: str, msg):
+        self.proxy.endpoints[method].send(msg)
+        self.proxy.endpoints[method].req_channel.transmit(100)
+
+
+###############################################################################
+#
+# Networks
+#
+###############################################################################
+class Network:
+    def __init__(self):
+        self.protocol = Protocol()
+        self.hosts = {}
+
+    def add_interface(self, ifc, methods):
+        self.protocol.add_interface(ifc, methods)
+
+    def add_method(self, ifc, m_name, one_way):
+        self.protocol.add_method(ifc, m_name, one_way)
+
+    def add_host(self, nid):
+        if not bool(self.protocol.interfaces):
+            raise TypeError("Interfaces must be added before host "
+                            "initialization")
+        self.hosts[nid] = Host(self)
+
+    @staticmethod
+    def link(src, dst):
+        src.connect_proxy(dst)
+
+
+class StarNetwork(Network):
+    def __init__(self, k, site_type, coord_type):
+        super().__init__()
+        self.site_type = site_type
+        self.coord_type = coord_type
+        self.coord = None
+
+    def add_coord(self):
+        self.hosts[None] = self.coord_type(self)
+        self.coord = self.hosts[None]
+
+    def add_site(self, nid, ifc):
+        self.hosts[nid] = self.site_type(self, ifc)
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,131 +1,23 @@
-import pytest
-
 from components import *
 
 
-def test_host():
-    echo_id: int = 1
-    h = Host(echo_id)
-
-    assert h.nid == echo_id
+def sample():
+    print("This is just a sample function")
 
 
-# ----------------------------------------------------------------------------
-def test_channel():
-    echo_src = Host(1)
-    echo_dst = Host(2)
-
-    c = Channel(echo_src, echo_dst)
-
-    assert c.src.nid == 1 and c.dst.nid == 2
+def sample2():
+    print("This is a second sample function")
 
 
-# ----------------------------------------------------------------------------
-def test_connect_1_node():
-    echo_net = StarNetwork(1, Host, Host)
-
-    assert len(echo_net.nodes) == 1 and echo_net.coord is not None
-
-    echo_node = echo_net.nodes[0]
-    echo_coord = echo_net.coord
-
-    assert echo_node.send_channels[echo_coord] == \
-           echo_coord.recv_channels[
-               echo_node], " Uplink is not created correctly"
+def test_create_protocol():
+    protocol = Protocol()
+    protocol.add_method(ifc_name="A", function=sample, one_way=True)
+    protocol.add_method(ifc_name="A", function=sample2, one_way=True)
+    assert len(protocol.interfaces["A"].methods) == 2
 
 
-# ----------------------------------------------------------------------------
-def echo_coord_handle_func(channel, msgtype, msg):
-    assert msg == "Hello coord"
-
-    coord_msg = "Thanks node" + str(channel.src.nid)
-    new_dst = channel.src
-    new_src = channel.dst
-
-    new_src.send(new_dst, msgtype, coord_msg)
-
-
-def echo_node_handle_func(channel, msgtype, msg):
-    assert msg == "Thanks node" + str(channel.dst.nid)
-
-
-def test_send_msg_1_node():
-    echo_net = StarNetwork(1, Host, Host)
-    echo_node = echo_net.nodes[0]
-    echo_coord = echo_net.coord
-
-    echo_msg = "Hello coord"
-    echo_type = "type"
-
-    echo_coord.add_handler(echo_type, echo_coord_handle_func)
-    echo_node.add_handler(echo_type, echo_node_handle_func)
-
-    echo_node.send(echo_coord, echo_type, echo_msg)
-
-
-# ----------------------------------------------------------------------------
-def test_send_many_msgs():
-    echo_net = StarNetwork(1, Host, Host)
-    echo_node = echo_net.nodes[0]
-    echo_coord = echo_net.coord
-
-    echo_type = "type"
-
-    echo_coord.add_handler(echo_type, echo_coord_handle_func)
-    echo_node.add_handler(echo_type, echo_node_handle_func)
-
-    for i in range(10):
-        echo_msg = "Hello coord"
-        echo_node.send(echo_coord, echo_type, echo_msg)
-        assert echo_node.send_channels[echo_coord].msg == i + 1
-
-
-# ----------------------------------------------------------------------------
-def test_broadcast_2_nodes():
-    echo_net = StarNetwork(2, Host, Host)
-
-    echo_msg = "Hello coord"
-    echo_type = "type"
-
-    echo_net.coord.add_handler(echo_type, echo_coord_handle_func)
-
-    for i in range(2):
-        assert echo_net.nodes[i].nid == i
-
-        echo_net.nodes[i].add_handler(echo_type, echo_node_handle_func)
-        echo_net.nodes[i].send(echo_net.coord, echo_type, echo_msg)
-
-
-# ----------------------------------------------------------------------------
-def iteration_node_handle(channel, msgtype, msg):
-    channel.src.store += msg
-    new_msg = channel.src.store
-
-    new_dst = channel.src
-    new_src = channel.dst
-
-    if msg < 11:
-        new_src.send(new_dst, msgtype, new_msg )
-
-
-def iteration_coord_handle(channel, msgtype, msg):
-    new_dst = channel.src
-    new_src = channel.dst
-
-    if msg != 10:
-        new_src.send(new_dst, msgtype, msg)
-    else:
-        assert msg == 10
-
-
-def test_iteration():
-
-    echo_net = StarNetwork(1, Host, Host)
-
-    echo_msg = 1
-    echo_type = "iteration"
-
-    echo_net.coord.add_handler(echo_type, iteration_coord_handle)
-    echo_net.nodes[0].add_handler(echo_type, iteration_node_handle)
-
-    echo_net.nodes[0].send(echo_net.coord, echo_type, echo_msg)
+def test_create_nodes():
+    net = StarNetwork(3)
+    assert len(net.hosts) == 1
+    assert len(net.groups) == 1
+    assert len(net.groups[3].members) == 3
